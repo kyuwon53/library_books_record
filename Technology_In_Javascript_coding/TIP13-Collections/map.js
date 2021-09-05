9c853394aaed124b5a2b8efdace68e4d0ae764d7
kyuwon53 <lkw2v@naver.com>
2021-09-05 22:40:43 +0900

    [Add] 맵 예제

---
+++ Technology_In_Javascript_coding/TIP13-Collections/map.js
@@ -0,0 +1,11 @@
+const petFilters = new Map();
+
+function addFilters(filters, key, value){
+  filters.set(key, value);
+}
+function deleteFilters(filters, key){
+  filters.delete(key);
+}
+function clearFilters(filters){
+  filters.clear();
+}
