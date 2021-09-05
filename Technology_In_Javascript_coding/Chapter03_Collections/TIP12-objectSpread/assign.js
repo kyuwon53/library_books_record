9947b1a729a759b9b31453cff0efeb9c0d19d471
kyuwon53 <lkw2v@naver.com>
2021-09-05 20:39:44 +0900

    [Add] TIP12 assign 예제

---
+++ Technology_In_Javascript_coding/Chapter03_Collections/TIP12-objectSpread/assign.js
@@ -0,0 +1,13 @@
+const defaults = {
+  author: '',
+  title: '',
+  year: 2017,
+  rating: null,
+};
+
+const book = {
+  author: 'Joe Morgan',
+  title: 'Simplifying JavaScript',
+};
+
+const updated = Object.assign({}, defaults, book);
