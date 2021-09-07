0867f020c1d803dc8bf654a2c03efafe6ba739c6
kyuwon53 <lkw2v@naver.com>
2021-09-07 21:21:12 +0900

    [Add] 맵 부수 효과 문제 해결 예제

---
+++ Technology_In_Javascript_coding/Chapter03_Collections/TIP15-MapSideEffects/copy.js
@@ -0,0 +1,10 @@
+function applyDefualts(map, defaults){
+  const copy = new Map([...map]);
+  for (const [key, value] of defaults){
+    if(!copy.has(key)){
+      copy.set(key, value);
+    }
+  }
+  return copy;
+}
+
