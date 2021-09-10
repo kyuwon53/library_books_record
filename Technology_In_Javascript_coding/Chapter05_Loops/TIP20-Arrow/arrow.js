a997e2615279a5f1447e0cb2f0a625ea0f6d0908
kyuwon53 <lkw2v@naver.com>
2021-09-10 22:13:50 +0900

    [Fix] 화살표 함수로 return 키워드 없애기

--- Technology_In_Javascript_coding/Chapter05_Loops/TIP20-Arrow/arrow.js
+++ Technology_In_Javascript_coding/Chapter05_Loops/TIP20-Arrow/arrow.js
@@ -9,3 +9,7 @@ const key = () => {
 const greet = (first, last) => {
   return `안녕하세요, ${capitalize(first)} ${capitalize(last)}님`;
 }
+
+const formatUser = name => {
+  return `${capitalize(name)}님이 로그인했습니다.`;
+}
