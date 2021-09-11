ad3dec692605f9d2e18dfce0a92790948e61b508
kyuwon53 <lkw2v@naver.com>
2021-09-11 12:50:50 +0900

    [Fix] filter() 함수를 이용한 부동소수점 함수

--- Technology_In_Javascript_coding/Chapter05_Loops/TIP21-Methods/methods.js
+++ Technology_In_Javascript_coding/Chapter05_Loops/TIP21-Methods/methods.js
@@ -1,2 +1,5 @@
 const prices = ['1.0', '2.15'];
 const formattedPrices = prices.map(price => parseFloat(price));
+
+const prices1 = ['1.0','흥정가능' ,'2.15'];
+const formattedPrices = prices.map(price => parseFloat(price)).filter(price => price);
