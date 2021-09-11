f7325a9c4a96dab0dadb8ebc816359f279fde37f
kyuwon53 <lkw2v@naver.com>
2021-09-11 13:21:38 +0900

    [Add] 연주자 목록이 담긴 컬렉션

---
+++ Technology_In_Javascript_coding/Chapter05_Loops/TIP22-map/map.js
@@ -0,0 +1,17 @@
+const band = [{
+    name: 'corbett',
+    instrument: 'guitar',
+  },
+  {
+    name: 'evan',
+    instrument: 'guitar',
+  },
+  {
+    name: 'sean',
+    instrument: 'bass',
+  },
+  {
+    name: 'brett',
+    instrument: 'drums',
+  },
+];
