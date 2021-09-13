7d9b332be644d445fb427fab5a2f19e4b5cf4d37
kyuwon53 <lkw2v@naver.com>
2021-09-13 20:58:50 +0900

    [Add] forEach()문 예제

---
+++ Technology_In_Javascript_coding/Chapter05_Loops/TIP24-ForEach/forEach2.js
@@ -0,0 +1,8 @@
+const names = ['walter', 'white'];
+let capitalized = [];
+names.forEach(name => capitalized.push(name.toUpperCase()));
+
+capitalized;
+// ['WALTER', 'WHITE']
+
+sailingClub.forEach(member => sendEmail(member));
