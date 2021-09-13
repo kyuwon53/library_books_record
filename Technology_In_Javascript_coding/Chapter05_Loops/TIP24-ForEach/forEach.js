7ea311d895232f1029f10437d5f7ccca5132b873
kyuwon53 <lkw2v@naver.com>
2021-09-13 20:58:16 +0900

    [Add] forEach()로 이메일 함수를 구현하라

---
+++ Technology_In_Javascript_coding/Chapter05_Loops/TIP24-ForEach/forEach.js
@@ -0,0 +1,15 @@
+const sailingClub = [
+  'yi hong',
+  'andy',
+  'darcy',
+  'jessi',
+  'alex',
+  'nathan',
+];
+
+const names = ['walter', 'white'];
+const capitalized = names.forEach(name => name.toUpperCase());
+
+capitalized;
+// undefined
+
