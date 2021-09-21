edf4e66f7563d50f1083afd57b9b74df59bc2e00
kyuwon53 <lkw2v@naver.com>
2021-09-21 17:25:18 +0900

    [Add] 의존성 주입을 사용한 코드

---
+++ Technology_In_Javascript_coding/Chapter07_Functions/TIP32-test/test.js
@@ -0,0 +1,6 @@
+function formatPrice(user, { price, location }, getTaxInformation){
+  const rate = getTaxInformation(location);
+  const taxes = rate ? `추가 세금 $${price * rate}` : '추가 세금';
+  return `${user}님의 합계 금액: $${price} 및 ${taxes}`;
+}
+export { formatPrice };
