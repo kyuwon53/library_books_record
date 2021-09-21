3c37fabd2735f22155bc23dcfe8f4f901846f66c
kyuwon53 <lkw2v@naver.com>
2021-09-21 22:25:32 +0900

    [Add] 고차 함수 화살표 함수

---
+++ Technology_In_Javascript_coding/Chapter07_Functions/TIP33-arrow/arrow.js
@@ -0,0 +1,30 @@
+const comic = {
+  first: 'Peter',
+  last: 'Bagge',
+  city: 'Seattle',
+  state: 'Washington',
+};
+
+const getName = ({ first, last }) => `${first} ${last}`;
+getName(comic);
+// Peter Bagge
+
+const getFullName = ({ first, last }) => ({ fullName: `${first} ${last}`});
+getFullName(comic);
+// { fullName: 'Peter Bagge' }
+
+const getNameAndLocation = ({first, last, city, state }) => ({
+  fullName: `${first} ${last}`,
+  location: `${city}, ${state}`,
+});
+getNameAndLocation(comic);
+// {
+//   fullName: 'Peter Bagge',
+//   location: 'Seattle, Washington'
+// }
+
+const discounter = discount => price => price * (1 - discount);
+
+const tenPercentOff = discounter(0.1);
+tenPercentOff(100);
+// 90
