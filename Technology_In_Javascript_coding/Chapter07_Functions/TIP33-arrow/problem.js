284d05e7e4255a141e204458878f55ff5634308a
kyuwon53 <lkw2v@naver.com>
2021-09-21 22:22:43 +0900

    [Add] 고차 함수 예제

--- Technology_In_Javascript_coding/Chapter07_Functions/TIP33-arrow/problem.js
+++ Technology_In_Javascript_coding/Chapter07_Functions/TIP33-arrow/problem.js
@@ -7,3 +7,11 @@ function getName({ first, last }){
   return `${first} ${last}`;
 }
 
+const discounter = discount => {
+  return price => {
+    return price * (1 - discount);
+  };
+};
+const tenPercentOff = discounter(0.1);
+tenPercentOff(100);
+// 90
