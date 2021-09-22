e6a575ee301895a493c50c7aabbbfa8a32a29ff8
kyuwon53 <lkw2v@naver.com>
2021-09-23 00:06:07 +0900

    [Add] 속성에 할당한 화살표 함수로 작성한 경우

--- Technology_In_Javascript_coding/Chapter07_Functions/TIP36-context/context.spec.js
+++ Technology_In_Javascript_coding/Chapter07_Functions/TIP36-context/context.spec.js
@@ -1,3 +1,7 @@
 validatorProblem.setInvalidMessages(field);
 // TypeError: Cannot read property 'message of undefined
 // 타입 오류: undefined의 속성 'message'를 읽을 수 없습니다.
+
+validatorMethod.setInvalidMessages(field);
+// TypeError: Cannot read property 'message' of undefined
+// 타입 오류: undefined의 속성 'message'를 읽을 수 없습니다.
