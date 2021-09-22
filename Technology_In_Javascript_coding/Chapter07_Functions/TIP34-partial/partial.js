7f2e3a533a60776bd9b99278756ebccc823c27e0
kyuwon53 <lkw2v@naver.com>
2021-09-22 14:42:41 +0900

    [Add] 데이터 예

---
+++ Technology_In_Javascript_coding/Chapter07_Functions/TIP34-partial/partial.js
@@ -0,0 +1,17 @@
+const building = {
+  hours : '8 a.m. - 8 p.m.',
+  address: 'Jayhawk Blvd',
+};
+const manager = {
+  name: 'Augusto',
+  phone: '555-555-5555',
+};
+const program = {
+  name: 'Presenting Research',
+  room: '415',
+  hours: '3-6',
+};
+const exhibit = {
+  name: 'Emerging Scholarship',
+  contact: 'Dyan',
+};
