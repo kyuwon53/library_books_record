1caefb68b65f64215a469d1c0274dabd33f8b912
kyuwon53 <lkw2v@naver.com>
2021-09-22 15:57:57 +0900

    [Add] 필터링 조건에 맞는 데이터를 반환하는 함수

---
+++ Technology_In_Javascript_coding/Chapter07_Functions/TIP35-curry/problem.js
@@ -0,0 +1,9 @@
+function getDogNames(dogs, filter){
+  const [key, value] = filter;
+  return dogs
+    .filter(dog => dog[key] === value)
+    .map(dog => dog['이름']);
+}
+
+getDogNames(dogs, ['색상', '검정색']);
+// ['맥스', '도니']
