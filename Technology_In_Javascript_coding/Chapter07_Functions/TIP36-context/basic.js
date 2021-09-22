c9619b3c80ff8a36e3ff80905d5530ec202c21fd
kyuwon53 <lkw2v@naver.com>
2021-09-22 23:38:03 +0900

    [Add] this 키워드 예제

---
+++ Technology_In_Javascript_coding/Chapter07_Functions/TIP36-context/basic.js
@@ -0,0 +1,9 @@
+const validator = {
+  message: '는 유효하지 않습니다.',
+  setInvalidMessage(field){
+    return `${field}${this.message}`;
+  },
+};
+
+validator.setInvalidMessage('도시');
+// 도시는 유효하지 않습니다. 
