9353061e724ec48048e097d665fba23e6ba6f568
kyuwon53 <lkw2v@naver.com>
2021-09-22 15:10:21 +0900

    [Fix] 매개변수로 하나만 사용하는 함수를 반환하라

--- Technology_In_Javascript_coding/Chapter07_Functions/TIP34-partial/partial.js
+++ Technology_In_Javascript_coding/Chapter07_Functions/TIP34-partial/partial.js
@@ -15,3 +15,35 @@ const exhibit = {
   name: 'Emerging Scholarship',
   contact: 'Dyan',
 };
+
+function mergeProgramInformation(building, manager){
+  const { hours, address } = building;
+  const { name, phone } = manager;
+  const defaults = {
+    hours,
+    address,
+    contact: name,
+    phone,
+  };
+
+  return program => {
+    return {...defaults, ...program };
+  };
+};
+
+const programInfo = mergeProgramInformation(building, manager)(program);
+// {
+//   name: 'Presenting Research',
+//   room: '415',
+//   hours: '3-6',
+//   address: 'Jayhawk Blvd',
+//   phone: '555-555-5555',
+// }
+const exhibitInfo = mergeProgramInformation(building, manager)(exhibit);
+// {
+//   name: 'Emerging Scholarship',
+//   contact: 'Dyan',  
+//   hours : '8 a.m. - 8 p.m.',
+//   address: 'Jayhawk Blvd',
+//   phone: '555-555-5555',
+// }
