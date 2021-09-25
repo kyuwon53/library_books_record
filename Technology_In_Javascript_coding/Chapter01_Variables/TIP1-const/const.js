af0052450d4c76f0ffeb71e84bc59fce04f29e57
kyuwon53 <lkw2v@naver.com>
2021-09-25 16:43:27 +0900

    [Fix] var 변수 선언

--- Technology_In_Javascript_coding/Chapter01_Variables/TIP1-const/const.js
+++ Technology_In_Javascript_coding/Chapter01_Variables/TIP1-const/const.js
@@ -3,3 +3,7 @@ const total = 100 + (100 * taxRate);
 // 100행의 코드를 건너뛰었습니다.
 return `구매 금액은 ${total}입니다. `;
 
+var taxRate = 0.1;
+var total = 100 + (100 * taxRate);
+// 100행의 코드를 건너뛰었습니다. 
+return `구매 금액은 ${total}입니다.`;
