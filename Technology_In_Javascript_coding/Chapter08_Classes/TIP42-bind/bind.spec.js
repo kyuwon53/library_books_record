dc8d3ea5b10bf715677f83e63e3f14c6c6dfae5d
kyuwon53 <lkw2v@naver.com>
2021-09-25 14:51:18 +0900

    [Add] bind()를 이용해 특정한 객체를 명시적으로 this로 설정

---
+++ Technology_In_Javascript_coding/Chapter08_Classes/TIP42-bind/bind.spec.js
@@ -0,0 +1,4 @@
+const Validator = new ValidatorProblem();
+Validator.setInvlidMessages('도시');
+// TypeError: Cannot read property 'messsage' of undefined
+// 타입 오류: undefined의 속성 'message'를 읽을 수 없습니다.
