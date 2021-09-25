0480e0d7b05181f667c3ca15709a49f3fe521813
kyuwon53 <lkw2v@naver.com>
2021-09-25 17:28:50 +0900

    [Add] 적절하게 const와 let을 사용하라

---
+++ Technology_In_Javascript_coding/Chapter01_Variables/TIP2-let/const.js
@@ -0,0 +1,15 @@
+function getLowestPrice(item){
+  const count = item.inventory;
+  let price = item.price;
+  if (item.salePrice){
+    const saleCount = item.saleInventory;
+    if (saleCount > 0){
+      price = item.salePrice;
+    }
+  }
+  if (count){
+    return price;
+  }
+
+  return 0;
+}
