b4e5b075c4cf8bf249c0abf4b75bbf3d48e272c5
kyuwon53 <lkw2v@naver.com>
2021-09-25 15:31:50 +0900

    [Add] 생성자에서 this에 연결

---
+++ Technology_In_Javascript_coding/Chapter08_Classes/TIP42-bind/constructor.js
@@ -0,0 +1,14 @@
+class Validator {
+  constructor(){
+    this.message = '가 유효하지 않습니다.';
+    this.setInvalidMessage = this.setInvalidMessage.bind(this);
+  }
+
+  setInvalidMessage(field){
+    return `${field}${this.message}`;
+  }
+
+  setInvalidMessages(...fields){
+    return fields.map(this.setInvalidMessage);
+  }
+}
