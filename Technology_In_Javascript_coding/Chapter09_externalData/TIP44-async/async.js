5bb88fdd84469d296d3acc051e1d7567fc8faf33
kyuwon53 <lkw2v@naver.com>
2021-09-26 22:54:17 +0900

    [Fix] 여러 개의 프라미스를 연결

--- Technology_In_Javascript_coding/Chapter09_externalData/TIP44-async/async.js
+++ Technology_In_Javascript_coding/Chapter09_externalData/TIP44-async/async.js
@@ -7,4 +7,15 @@ getTheme()
   .then(theme => {
     console.log(theme);
   });
-  
+
+async function getArtistByPreference() {
+  const { theme } = await getUserPreferences();
+  const { album } = await getMusic(theme);
+  const { artist } = await getArtist(album);
+  return artist;
+}
+
+getArtistByPreference()
+  .then(artist => {
+    console.log(artist);
+  });
