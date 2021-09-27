fb6da1edd1cb2f9e2f31efc73bea9b5b54c701e1
kyuwon53 <lkw2v@naver.com>
2021-09-27 22:05:47 +0900

    [Add] ok필드로 응답확인, 오류 처리

--- Technology_In_Javascript_coding/Chapter09_externalData/TIP45-fetch/fetch.js
+++ Technology_In_Javascript_coding/Chapter09_externalData/TIP45-fetch/fetch.js
@@ -15,3 +15,16 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
     console.log(post.title);
   });
 
+fetch('https://jsonplaceholder.typicode.com/pots/1')
+  .then(data => {
+    if (!data.ok){
+      throw Error(data.status);
+    }
+    return data.json();
+  })
+  .then(post => {
+    console.log(post.title);
+  })
+  .catch(e => {
+    console.log(e);
+  });
