2b28e15b3b8467661715c403a282ffa5975e863a
kyuwon53 <lkw2v@naver.com>
2021-09-27 22:32:16 +0900

    [Add] POST 요청 JSON 데이터

--- Technology_In_Javascript_coding/Chapter09_externalData/TIP45-fetch/fetch.js
+++ Technology_In_Javascript_coding/Chapter09_externalData/TIP45-fetch/fetch.js
@@ -28,3 +28,35 @@ fetch('https://jsonplaceholder.typicode.com/pots/1')
   .catch(e => {
     console.log(e);
   });
+
+const update = {
+  title: 'Clarence White Techniques',
+  body: 'Amazing',
+  userId: 1,
+};
+
+const options = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(update),
+};
+
+fetch('https://jsonplaceholder.typicode.com/posts',options).
+then(data => {
+  if (!data.ok) {
+    throw Error(data.status);
+  }
+  return data.json();
+}).then(update => {
+  console.log(update);
+  // {
+  //   title: 'Clarence White Techniques',
+  //   body: 'Amazing',
+  //   userId: 1,
+  //   id: 101
+  // };
+}).catch(e => {
+  console.log(e);
+});
