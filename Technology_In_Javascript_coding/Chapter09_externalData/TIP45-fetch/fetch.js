e207500236b73baf9b19059d7886d55a30dbfa5e
kyuwon53 <lkw2v@naver.com>
2021-09-27 22:00:32 +0900

    [Add] then() 메서드의 콜백에서 파싱된 데이터를 처리

--- Technology_In_Javascript_coding/Chapter09_externalData/TIP45-fetch/fetch.js
+++ Technology_In_Javascript_coding/Chapter09_externalData/TIP45-fetch/fetch.js
@@ -6,3 +6,12 @@ fetch('https://jsonplaceholder.typicode.com/posts/1');
 //   title: 'First Post',
 //   body: 'This is my first post...',
 // }
+
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(data => {
+    return data.json();
+  })
+  .then(post => {
+    console.log(post.title);
+  });
+
